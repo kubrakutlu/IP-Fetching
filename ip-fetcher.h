da69461cec7ba3b5e99eeaa03b883e2413a9b09d
Kübra Kutlu <kubrakutlu9@gmail.com>
2019-08-15 14:02:49 +0300

    Update ip-fetcher.h

--- ip-fetcher.h
+++ ip-fetcher.h
@@ -1,8 +1,9 @@
+Copyright (c) 2015-2016  Joachim Nilsson <troglobit@gmail.com>
+
 #ifndef IP_FETCHER_H
 #define IP_FETCHER_H
 
 #include <stdio.h>
-//#include <widget.h>
 #include <sys/socket.h>
 
 int connectIpify(void);
